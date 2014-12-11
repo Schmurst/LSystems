eb1edef19bce57a8d0dddfb9d06c86195ee9756c
Sam <samhayhurst@gmail.com>
2014-12-11 01:26:31 +0000

    updated to now use a single 3D mesh for the tree, miantains same functionality

--- LSystems.h
+++ LSystems.h
@@ -13,64 +13,37 @@ namespace octet {
   class LSystems : public app {
   private:
     // Debug bools
-    enum { LS_DEBUG_PARSER = 1, LS_DEBUG_ITERATE = 1, CHINA = 10000 };
+    enum { LS_DEBUG_PARSER = 1, LS_DEBUG_ITERATE = 1};
 
     // scene for drawing box
     ref<visual_scene> app_scene;
     camera_instance *camera;
-    material *red;
+    material *mat;
 
-    // L system trees
-    ref<L_system> tree_1, tree_2, tree_3, tree_4, tree_5, tree_6;
+    ref<L_system> tree;
+    dynarray<string> FILENAMES;
 
-    dynarray<ref<L_system>> lsystemStack;
-
-    void load_L_Systems(){
-
-      // Make the first tree
-      tree_1 = new L_system();
-      tree_1->loadFile("assets/Lsystems/Tree1.txt");
-      printf("The length of the final string is: %i\n", tree_1->getAxiomSize());
-      tree_1->calculateVertices();
-      lsystemStack.push_back(tree_1);
-
-      // Make the 2nd tree
-      tree_2 = new L_system();
-      tree_2->loadFile("assets/Lsystems/Tree2.txt");
-      printf("The length of the final string is: %i\n", tree_2->getAxiomSize());
-      tree_2->calculateVertices();
-      lsystemStack.push_back(tree_2);
-
-      // Make the 3nd tree
-      tree_3 = new L_system();
-      tree_3->loadFile("assets/Lsystems/Tree3.txt");
-      printf("The length of the final string is: %i\n", tree_3->getAxiomSize());
-      tree_3->calculateVertices();
-      lsystemStack.push_back(tree_3);
-
-      // Make the 4th tree
-      tree_4 = new L_system();
-      tree_4->loadFile("assets/Lsystems/Tree4.txt");
-      printf("The length of the final string is: %i\n", tree_4->getAxiomSize());
-      tree_4->calculateVertices();
-      lsystemStack.push_back(tree_4);
-
-      // Make the 5th tree
-      tree_5 = new L_system();
-      tree_5->loadFile("assets/Lsystems/Tree5.txt");
-      printf("The length of the final string is: %i\n", tree_5->getAxiomSize());
-      tree_5->calculateVertices();
-      lsystemStack.push_back(tree_5);
-
-      // Make the 6th tree
-      tree_6 = new L_system();
-      tree_6->loadFile("assets/Lsystems/Tree6.txt");
-      printf("The length of the final string is: %i\n", tree_6->getAxiomSize());
-      tree_6->calculateVertices();
-      lsystemStack.push_back(tree_6);
-
+    void setFileNames(){
+      FILENAMES.push_back("assets/Lsystems/Tree1.txt");
+      FILENAMES.push_back("assets/Lsystems/Tree2.txt");
+      FILENAMES.push_back("assets/Lsystems/Tree3.txt");
+      FILENAMES.push_back("assets/Lsystems/Tree4.txt");
+      FILENAMES.push_back("assets/Lsystems/Tree5.txt");
+      FILENAMES.push_back("assets/Lsystems/Tree6.txt");
+      FILENAMES.push_back("assets/Lsystems/Tree7.txt");
+      FILENAMES.push_back("assets/Lsystems/Tree8.txt");
     }
 
+    void loadNewTree(int index){
+      tree = new L_system();
+      tree->loadFile(FILENAMES[index]);
+      tree->iteration(1);
+      tree->initialiseDrawParams();
+      tree->interpret_axiom();
+      scene_node * testNode = tree->getNode();
+      app_scene->add_child(testNode);
+      app_scene->get_mesh_instance(0)->set_mesh(tree->getMesh());
+    }
 
   public:
     /// this is called when we construct the class before everything is initialised.
@@ -87,21 +60,19 @@ namespace octet {
       app_scene->get_camera_instance(0)->get_node()->access_nodeToParent().translate(vec3(0, 10, 10));
 
       param_shader *shader = new param_shader("shaders/default.vs", "shaders/simple_color.fs");
-      red = new material(vec4(1, 0, 0.6f, 1), shader);
+      mat = new material(vec4(1, 0, 0, 1), shader);
 
-      load_L_Systems();
+      setFileNames();
 
-      // Place the target tree in the scene
-      for (int i = 0; i < lsystemStack.size(); ++i){
-        scene_node * testNode = lsystemStack[i]->getNode();
-        app_scene->add_child(testNode);
-        app_scene->add_mesh_instance(new mesh_instance(testNode, lsystemStack[i]->getMesh(), red));
-        (i != 1) ? lsystemStack[i]->moveAway() : lsystemStack[i]->moveBack();
-      }
+      tree = new L_system();
+      tree->loadFile(FILENAMES[0]);
+      tree->iteration(4);
+      tree->initialiseDrawParams();
+      tree->interpret_axiom();
+      scene_node * testNode = tree->getNode();
+      app_scene->add_child(testNode);
+      app_scene->add_mesh_instance(new mesh_instance(testNode, tree->getMesh(), mat));
 
-     
-      // set the line width
-      glLineWidth(1.0f);
     }
 
     /// this is called to draw the world
@@ -116,60 +87,38 @@ namespace octet {
       // draw the scene
       app_scene->render((float)vx / vy);
 
-      // iterate up
-      if (is_key_going_down('O')){
-        for (int i = 0; i < lsystemStack.size(); i++){
-          if (lsystemStack[i]->is_in_view() == true){
-            lsystemStack[i]->incrementIteration();
-            lsystemStack[i]->calculateVertices();
-          }
+      scene_node *node = app_scene->get_mesh_instance(0)->get_node();
+      node->rotate(1, vec3(0, 1, 0));
+      node->transform(vec3(0));
+
+      for (int i = 1; i < 8; ++i){
+        if (is_key_going_down((char)(((int)'0') + i))){
+          loadNewTree(i - 1);
         }
       }
 
-      // iterate down
+      if (is_key_going_down('O')){
+        tree->incrementIteration();
+      }
+
       if (is_key_going_down('P')){
-        for (int i = 0; i < lsystemStack.size(); i++){
-          if (lsystemStack[i]->is_in_view() == true){
-            lsystemStack[i]->decrementIteration();
-            lsystemStack[i]->calculateVertices();
-          }
-        }
+        tree->decrementIteration();
       }
 
       if (is_key_down('K')){
-        for (int i = 0; i < lsystemStack.size(); i++){
-          if (lsystemStack[i]->is_in_view() == true){
-            lsystemStack[i]->incrementAngle();
-            lsystemStack[i]->calculateVertices();
-          }
-        }
+        tree->incrementAngle();
       }
 
       if (is_key_down('L')){
-        for (int i = 0; i < lsystemStack.size(); i++){
-          if (lsystemStack[i]->is_in_view() == true){
-            lsystemStack[i]->decrementAngle();
-            lsystemStack[i]->calculateVertices();
-          }
-        }
+        tree->decrementAngle();
       }
 
       if (is_key_down('N')){
-        for (int i = 0; i < lsystemStack.size(); i++){
-          if (lsystemStack[i]->is_in_view() == true){
-            lsystemStack[i]->incrementTranslation();
-            lsystemStack[i]->calculateVertices();
-          }
-        }
+        tree->incrementTranslation();
       }
 
       if (is_key_down('M')){
-        for (int i = 0; i < lsystemStack.size(); i++){
-          if (lsystemStack[i]->is_in_view() == true){
-            lsystemStack[i]->decrementTranslation();
-            lsystemStack[i]->calculateVertices();
-          }
-        }
+        tree->decrementTranslation();
       }
 
       if (is_key_down('Q')){
@@ -181,11 +130,11 @@ namespace octet {
       }
 
       if (is_key_down('W')){
-        camera->get_node()->translate(vec3(0, 0, 1.0f));
+        camera->get_node()->translate(vec3(0, 0, -1.0f));
       }
 
       if (is_key_down('S')){
-        camera->get_node()->translate(vec3(0, 0, -1.0f));
+        camera->get_node()->translate(vec3(0, 0, 1.0f));
       }
 
       if (is_key_down('A')){
@@ -194,60 +143,6 @@ namespace octet {
 
       if (is_key_down('D')){
         camera->get_node()->translate(vec3(1.0f, 0, 0));
-      }
-
-      if (is_key_going_down('1')){
-        if (lsystemStack[0]->is_in_view()){
-          lsystemStack[0]->moveAway();
-        }
-        else{
-          lsystemStack[0]->moveBack();
-        }        
-      }
-
-      if (is_key_going_down('2')){
-        if (lsystemStack[1]->is_in_view()){
-          lsystemStack[1]->moveAway();
-        }
-        else{
-          lsystemStack[1]->moveBack();
-        }
-      }
-
-      if (is_key_going_down('3')){
-        if (lsystemStack[2]->is_in_view()){
-          lsystemStack[2]->moveAway();
-        }
-        else{
-          lsystemStack[2]->moveBack();
-        }
-      }
-
-      if (is_key_going_down('4')){
-        if (lsystemStack[3]->is_in_view()){
-          lsystemStack[3]->moveAway();
-        }
-        else{
-          lsystemStack[3]->moveBack();
-        }
-      }
-
-      if (is_key_going_down('5')){
-        if (lsystemStack[4]->is_in_view()){
-          lsystemStack[4]->moveAway();
-        }
-        else{
-          lsystemStack[4]->moveBack();
-        }
-      }
-
-      if (is_key_going_down('6')){
-        if (lsystemStack[5]->is_in_view()){
-          lsystemStack[5]->moveAway();
-        }
-        else{
-          lsystemStack[5]->moveBack();
-        }
       }
     }
   };
--- L_system.h
+++ L_system.h
@@ -25,26 +25,26 @@ namespace octet{
     };
 
   private:
-    // char array containing the entire contents of the L systems instruction file.
+
+    // l System variables
     int fileSize;
     octet::string message;            // contains the message
     dynarray<uint8_t> lSystemFile;    // containts the entire file
     dynarray<char> alphabet;          // contains the alphabet
     dynarray<char> axiom;             // contains the axiom
-    dynarray<char> startingAxiom;     // axiom from file
+    char startingAxiom;               // axiom from file
     hash_map<char, string> rules;     // contains the rules
     float angle;                      // angle used for rotation
-    float chinaTranslation;
-    
-    bool in_view = true;
-    int currentItr = 1;
+    int iteration_count;
 
     // drawing variables
     ref<scene_node> node;
     ref<mesh> _mesh;
     myVertex *vtx;
+    int numVtxs;
+    uint32_t *idx;
     dynarray<mat4t> placementStack;
-    vec3 translateF = vec3(0, 0.2f, 0);
+    vec3 translateF = vec3(0, 1.0f, 0);
 
     // this function converts three floats into a RGBA 8 bit color, taken from Andy's geometry example
     static uint32_t make_color(float r, float g, float b) {
@@ -101,35 +101,7 @@ namespace octet{
       }
       axiom.resize(new_array.size());
       memcpy(axiom.data(), new_array.data(), new_array.size());
-    
-    }
-
-    /// This fucntion sets up th mesh to be drawn, taken and edited from Andy's geometry example
-    void initialiseDrawParams() {
-
-      int num = 0;
-      for (int i = 0; i < axiom.size(); ++i){
-        if (axiom[i] == 'F') {
-          ++num;
-        }
-      }
-
-        _mesh->allocate(sizeof(myVertex) * num * 2, 0);
-        _mesh->set_params(sizeof(myVertex), 0, num * 2, GL_LINES, NULL);
-
-      // describe the structure of my_vertex to OpenGL
-      if (_mesh->get_num_slots() < 2){
-        _mesh->add_attribute(attribute_pos, 3, GL_FLOAT, 0);
-        _mesh->add_attribute(attribute_color, 4, GL_UNSIGNED_BYTE, 12, GL_TRUE);
-      }
-
-      // these write-only locks give access to the vertices and indices
-      gl_resource::wolock vertexLock(_mesh->get_vertices());
-      vtx = (myVertex *)vertexLock.u8();
-
-      // insert some deafult drawing values
-      mat4t matrix;
-      placementStack.push_back(matrix);
+      ++iteration_count;
     }
 
     /// This function constructs the rules for the Lsyatem from the file
@@ -178,8 +150,10 @@ namespace octet{
 
       for (int i = a; i < b; i++){
         if (lSystemFile[i] != ',')
-          startingAxiom.push_back(lSystemFile[i]);
+          startingAxiom = lSystemFile[i];
       }
+
+      axiom.push_back(startingAxiom);
 
       printf("Here is the Axiom: %.*s\n", axiom.size(), axiom.data());
 
@@ -215,25 +189,149 @@ namespace octet{
       if (LS_DEBUG_PARSER) printf("The Angle is: %g\n", angle);
     }
 
-    /// reset the axiom to starting axiom
-    void resetAxiom(){
-      axiom.reset();
-      axiom.resize(startingAxiom.size());
-      for (int i = 0; i < startingAxiom.size(); i++){
-        axiom[i] = startingAxiom[i];
-      }
-    }
-
   public:
 
     /// default constructer
     L_system(){
-      // scene node
       node = new scene_node();
-      chinaTranslation = 0;
-      // allocate vertices and indices into OpenGL buffers
       _mesh = new mesh();
+      placementStack.push_back(mat4t());
+      numVtxs = 0;
+      iteration_count = 0;
     }
+
+    /// These functions are to be used with the new framework ----------------------
+
+    /// this function calculates the vertices of a prism given a matrix from the matrix stack 
+    /// This code has been taken from Andy's geometery example and modified
+    void calculate_prism_vertices(mat4t &placement){
+
+      // number of steps in tri-prism
+      size_t num_steps = 3;
+
+      vec3 pos0 = placement[3].xyz();
+      placement.translate(translateF);
+      vec3 pos1 = placement[3].xyz();
+
+      // make the vertices
+      float radius = 0.2f;
+
+      for (size_t i = 0; i < num_steps; ++i) {
+        float r = 0.0f, g = 1.0f * i / num_steps, b = 1.0f;
+        float theta = i * 2.0f * 3.14159265f / num_steps;
+        vtx->pos = pos0 + vec3p(cosf(theta) * radius, 0, sinf(theta) * radius);
+        // printf("vtx bot %i: %f, %f, %f  ", i, vtx->pos.x(), vtx->pos.y(), vtx->pos.z());
+        vtx->colour = make_color(r, g, b);
+        vtx++;
+        vtx->pos = pos1 + vec3p(cosf(theta) * radius, 0, sinf(theta) * radius);
+        // printf("vtx top %i: %f, %f, %f\n", i, vtx->pos.x(), vtx->pos.y(), vtx->pos.z());
+        vtx->colour = make_color(r, g, b);
+        vtx++;
+      }
+
+ 
+      // make the triangles
+      uint32_t vn = 0;
+      for (size_t i = 0; i != num_steps; ++i) {
+        /*
+        1---------3
+        |       / |
+        |    /    |   
+        | /       |
+        0---------2
+        */   
+
+        idx[0] = vn + 0 + numVtxs;
+        idx[1] = ((vn + 3) % 6) + numVtxs;
+        idx[2] = vn + 1 + numVtxs;
+        idx += 3;
+        // printf("%u,%u,%u :", idx[-3], idx[-2], idx[-1]);
+        idx[0] = vn + 0 + numVtxs;
+        idx[1] = ((vn + 2) % 6) + numVtxs;
+        idx[2] = ((vn + 3) % 6) + numVtxs;
+        idx += 3;
+       // printf(" %u,%u,%u\n", idx[-3], idx[-2], idx[-1]);
+        vn += 2;
+      }
+      // this is number of vertices added
+      numVtxs += 6;
+
+    }
+
+    /// This function interprets the axiom to populate the mesh
+    void interpret_axiom(){
+
+      numVtxs = 0;
+      mat4t matrix;
+      
+      // for each char in axiom do x
+      for each (char c in axiom)
+      {
+        switch (c)
+        {
+        case 'F':
+          // draw a prism
+          calculate_prism_vertices(placementStack.back());
+          
+          break;
+        case '[':
+          // push a matrix onto the stack
+          matrix = placementStack.back();
+          placementStack.push_back(matrix);
+          break;
+        case ']':
+          // pop a matrix off the stack
+          placementStack.pop_back();
+          break;
+        case '+':
+          // rotate around z +ve
+          placementStack.back().rotateZ(angle);
+          break;
+        case '-':
+          // rotate around z -ve
+          placementStack.back().rotateZ(-angle);
+          break;
+        default:
+          break;
+        }
+        
+      }
+    }
+
+    /// This fucntion sets up th mesh to be drawn, taken and edited from Andy's geometry example
+    void initialiseDrawParams() {
+
+      int num = 0;
+      for (int i = 0; i < axiom.size(); ++i){
+        if (axiom[i] == 'F') {
+          ++num;
+        }
+      }
+
+      _mesh->init();
+
+      int num_steps = 3;
+      // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+      // allocate vertices and indices into OpenGL buffers
+      size_t num_vertices = num * 6;
+      size_t num_indices = num * 6 * num_steps;
+      _mesh->allocate(sizeof(myVertex) * num_vertices, sizeof(uint32_t) * num_indices);
+      _mesh->set_params(sizeof(myVertex), num_indices, num_vertices, GL_TRIANGLES, GL_UNSIGNED_INT);
+
+      // describe the structure of my_vertex to OpenGL
+      _mesh->add_attribute(attribute_pos, 3, GL_FLOAT, 0);
+      _mesh->add_attribute(attribute_color, 4, GL_UNSIGNED_BYTE, 12, GL_TRUE);
+
+
+      // these write-only locks give access to the vertices and indices.
+      // they will be released at the next } (the end of the scope)
+      gl_resource::wolock vl(_mesh->get_vertices());
+      vtx = (myVertex *)vl.u8();
+      gl_resource::wolock il(_mesh->get_indices());
+      idx = il.u32();
+    }
+
+    /// ----------------------------------------------------------------------------
 
     /// Parser function, used to read all info from txt file
     void loadFile(string name){
@@ -244,7 +342,6 @@ namespace octet{
       fileSize = lSystemFile.size();
 
       removeWhiteSpace(lSystemFile);
-
       constructLSystem();
     }
 
@@ -255,70 +352,8 @@ namespace octet{
       }
     }
 
-    /// This function hacks the mesh to another position
-    void moveAway(){
-      if (in_view){
-        chinaTranslation = 1000000;
-        calculateVertices();
-        in_view = false;
-      }
-    }
+    /// Get functions below /// ==================================================================
 
-    /// This function hacks the mesh to the camera position
-    void moveBack(){
-      if (!in_view){
-        chinaTranslation = 0;
-        calculateVertices();
-        in_view = true;
-      }
-    }
-
-    /// This function loops through the current axiom and adds vertexs to the mesh
-    void calculateVertices(){
-
-      resetAxiom();
-      iteration(currentItr);
-      initialiseDrawParams();
-
-      mat4t matrix;
-      for (int i = 0; i < axiom.size(); i++)
-      {
-        switch (axiom[i])
-        {
-        case 'F':
-          if (LS_DEBUG_ITERATE) printf("Letter recognised as %c\n", axiom[i]);
-          vtx->pos = vec3(placementStack.back()[3].xyz()[0] + chinaTranslation, placementStack.back()[3].xyz()[1], placementStack.back()[3].xyz()[2]);
-          vtx++;
-          placementStack.back().translate(translateF);
-          vtx->pos = vec3(placementStack.back()[3].xyz()[0] + chinaTranslation, placementStack.back()[3].xyz()[1], placementStack.back()[3].xyz()[2]);
-          vtx++;
-          break;
-        case '[':
-          if (LS_DEBUG_ITERATE) printf("Letter recognised as %c\n", axiom[i]);
-          matrix = placementStack.back();
-          placementStack.push_back(matrix);
-          break;
-        case ']':
-          if (LS_DEBUG_ITERATE) printf("Letter recognised as %c\n", axiom[i]);
-          placementStack.pop_back();
-          break;
-        case '+':
-          if (LS_DEBUG_ITERATE) printf("Letter recognised as %c\n", axiom[i]);
-          placementStack.back().rotateZ(angle);
-          break;
-        case '-':
-          if (LS_DEBUG_ITERATE) printf("Letter recognised as %c\n", axiom[i]);
-          placementStack.back().rotateZ(-angle);
-          break;
-        default:
-          if (LS_DEBUG_ITERATE) printf("Letter not recognised: %c, therefore doing nothing\n", axiom[i]);
-          break;
-        }
-      }
-    }
-
-    /// Get functions below /// ---------------------------------------------------
-    
     /// returns axiom's size
     int getAxiomSize(){
       return axiom.size();
@@ -334,49 +369,63 @@ namespace octet{
       return _mesh;
     }
 
-    /// get current iteration level
-    int getCurrentIteration(){
-      return currentItr;
-    }
-
-    /// sets current int
-    void setCurrentIteration(int i){
-      currentItr = i;
-    }
-
     /// increments current iterations
     void incrementIteration(){
-      currentItr++;
+      placementStack.reset();
+      placementStack.push_back(mat4t());
+      iterate();
+      initialiseDrawParams();
+      interpret_axiom();
     }
 
     /// deccrements current iterations
     void decrementIteration(){
-      (currentItr != 0) ? --currentItr : currentItr = 0;
+      int target = (iteration_count != 1) ? iteration_count - 1 : 0;
+      iteration_count = 0;
+      axiom.reset();
+      axiom.resize(1);
+      axiom.push_back(startingAxiom);
+      placementStack.reset();
+      placementStack.push_back(mat4t());
+      iteration(target);
+      initialiseDrawParams();
+      interpret_axiom();
     }
 
     /// increment angle by 1degree
     void incrementAngle(){
       angle += 1.0f;
+      placementStack.reset();
+      placementStack.push_back(mat4t());
+      initialiseDrawParams();
+      interpret_axiom();
     }
 
     /// decrment angle by 1.0f degrees
     void decrementAngle() {
       angle -= 1.0f;
+      placementStack.reset();
+      placementStack.push_back(mat4t());
+      initialiseDrawParams();
+      interpret_axiom();
     }
 
     /// increment the translation magnitude
     void incrementTranslation(){
       translateF[1] += 0.02f;
+      placementStack.reset();
+      placementStack.push_back(mat4t());
+      initialiseDrawParams();
+      interpret_axiom();
     }
 
     /// increment the translation magnitude
     void decrementTranslation(){
       translateF[1] -= 0.02f;
-    }
-
-    /// checks whether the tree is in view
-    bool is_in_view(){
-      return in_view;
+      placementStack.reset();
+      placementStack.push_back(mat4t());
+      initialiseDrawParams();
+      interpret_axiom();
     }
   };
 }
